a34f29f68e59ff47504c8414c05d3b5d1193056b
xiaowuruan <xiaowuruan@tencent.com>
2026-03-09 18:56:11 +0800

    feat: update version

--- huxuan-auto-login.user.js
+++ huxuan-auto-login.user.js
@@ -2,7 +2,7 @@
 // @name         互选官网自动登录
 // @namespace    https://huxuan.qq.com/
 // @icon         https://file.daihuo.qq.com/fe_free_trade/favicon.png
-// @version      1.0.6
+// @version      1.0.7
 // @description  自动完成互选官网的 QQ 密码登录流程，支持配置账号、密码和目标账户 ID
 // @author       Huxuan AutoLogin
 // @homepageURL  https://github.com/xiaowulang-turbo/Huxuan-AutoLogin
